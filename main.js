1ecb9c8b152729b8fdb823d9dd0c1393f208e4cc
‘notilttoday’ <tsimbaloleg2006@gmail.com>
2024-10-12 00:12:05 +0300

    PR3-3 first

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const $btnKickCharacter1 = document.getElementById('btn-kick-character-1');
 const $btnKickCharacter2 = document.getElementById('btn-kick-character-2');
+const $logs = document.querySelector('#logs');
 
 const character = {
     name: 'Zapdos',
@@ -8,27 +9,34 @@ const character = {
     elHP: document.getElementById('health-character'),
     elProgressbar: document.getElementById('progressbar-character'),
 
-    renderHP: function() {
+    renderHP() {
         this.renderHPLife();
         this.renderProgressbarHP();
     },
 
-    renderHPLife: function() {
-        this.elHP.innerText = this.damageHP + ' / ' + this.defaultHP;
+    renderHPLife() {
+        this.elHP.innerText = `${this.damageHP} / ${this.defaultHP}`;
     },
 
-    renderProgressbarHP: function() {
-        this.elProgressbar.style.width = (this.damageHP / this.defaultHP) * 100 + '%';
+    renderProgressbarHP() {
+        this.elProgressbar.style.width = `${(this.damageHP / this.defaultHP) * 100}%`;
     },
     
-    changeHP: function(count) {
+    changeHP(count) {
         this.damageHP -= count;
+
         if (this.damageHP < 0) {
             this.damageHP = 0;
             alert('Бідний ' + this.name + ' програв бій!');
             $btnKickCharacter1.disabled = true;
             $btnKickCharacter2.disabled = true;
         }
+
+        const $p = document.createElement('p');
+        const log = this === enemy ? generateLog(this, character) : generateLog(this, enemy);
+        $p.innerHTML = `${log} <span style="color: yellow">-${count} [${this.damageHP} / ${this.defaultHP}]</span>`;
+        $logs.insertBefore($p, $logs.children[0]);
+
         this.renderHP();
     }
 };
@@ -40,33 +48,52 @@ const enemy = {
     elHP: document.getElementById('health-enemy'),
     elProgressbar: document.getElementById('progressbar-enemy'),
 
-    renderHP: function() {
+    renderHP() {
         this.renderHPLife();
         this.renderProgressbarHP();
     },
-    
-    renderHPLife: function() {
-        this.elHP.innerText = this.damageHP + ' / ' + this.defaultHP;
+
+    renderHPLife() {
+        this.elHP.innerText = `${this.damageHP} / ${this.defaultHP}`;
     },
 
-    renderProgressbarHP: function() {
-        this.elProgressbar.style.width = (this.damageHP / this.defaultHP) * 100 + '%';
+    renderProgressbarHP() {
+        this.elProgressbar.style.width = `${(this.damageHP / this.defaultHP) * 100}%`;
     },
 
-    changeHP: function(count) {
+    changeHP(count) {
         this.damageHP -= count;
+
         if (this.damageHP < 0) {
             this.damageHP = 0;
             alert('Бідний ' + this.name + ' програв бій!');
             $btnKickCharacter1.disabled = true;
             $btnKickCharacter2.disabled = true;
         }
+
+        const $p = document.createElement('p');
+        const log = this === enemy ? generateLog(this, character) : generateLog(this, enemy);
+        $p.innerHTML = `${log} <span style="color: red">-${count} [${this.damageHP} / ${this.defaultHP}]</span>`;
+        $logs.insertBefore($p, $logs.children[0]);
+
         this.renderHP();
     }
 };
 
-function attack(attacker, target, damage) {
-    target.changeHP(damage);
+function generateLog({ name: firstPersonName }, { name: secondPersonName }) {
+    const logs = [
+        `${firstPersonName} згадав щось важливе, але несподівано ${secondPersonName}, не пам'ятаючи себе від переляку, вдарив у передпліччя ворога.`,
+        `${firstPersonName} поперхнувся, і за це ${secondPersonName} з переляку приклав прямий удар коліном у лоб ворога.`,
+        `${firstPersonName} забувся, але в цей час нахабний ${secondPersonName}, прийнявши вольове рішення, нечутно підійшовши ззаду, вдарив.`,
+        `${firstPersonName} прийшов до тями, але несподівано ${secondPersonName} випадково завдав потужного удару.`,
+        `${firstPersonName} поперхнувся, але в цей час ${secondPersonName} неохоче розтрощив кулаком <вирізано цензурою> супротивника.`,
+        `${firstPersonName} здивувався, а ${secondPersonName} похитнувшись вліпив підлий удар.`,
+        `${firstPersonName} висморкався, але несподівано ${secondPersonName} провів подрібнюючий удар.`,
+        `${firstPersonName} похитнувся, і раптово нахабний ${secondPersonName} без причини вдарив у ногу супротивника.`,
+        `${firstPersonName} засмутився, як раптом, несподівано ${secondPersonName} випадково вліпив стопою в живіт суперника.`,
+        `${firstPersonName} намагався щось сказати, але раптом, несподівано ${secondPersonName} з нудьги, розбив брову супернику.`
+    ];
+    return logs[random(logs.length) - 1];
 }
 
 function init() {
@@ -80,17 +107,17 @@ function random(num) {
 }
 
 $btnKickCharacter1.addEventListener('click', function () {
-    const damage = random(30);
-    attack(character, enemy, damage);
-    const enemyDamage = random(30);
-    attack(enemy, character, enemyDamage);
+    const damage1 = random(30);
+    const damage2 = random(30);
+
+    enemy.changeHP(damage2);
+    character.changeHP(damage1);
 });
 
 $btnKickCharacter2.addEventListener('click', function () {
     const damage = 10;
-    attack(enemy, character, damage);
-    const characterDamage = 10;
-    attack(character, enemy, characterDamage);
+    enemy.changeHP(damage);
+    character.changeHP(damage);
 });
 
 init();
